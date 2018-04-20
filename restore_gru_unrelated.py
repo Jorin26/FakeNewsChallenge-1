56ff39255b5b7708f838db09044ec04a38b761e4
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-04-20 15:45:55 -0400

    Clean repository

--- restore_gru_unrelated.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-with tf.Session() as sess:
-    saver = tf.train.import_meta_graph('./gru_related_ckpt.meta')
-    saver.restore(sess, tf.train.latest_checkpoint('./'))
-    print(sess.run(tf.global_variables()))
